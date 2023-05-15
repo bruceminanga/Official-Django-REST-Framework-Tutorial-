c942c674f0e1042808c89720636310ff7aa7470d
macurrent <bruceminanga@gmail.com>
2023-05-15 16:48:23 +0300

    first commit

---
+++ tutorial/urls.py
@@ -0,0 +1,8 @@
+# tutorial/urls.py
+from django.contrib import admin
+from django.urls import include, path  # new
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("snippets.urls")),  # new
+]
